16f36e0b4f9df5bddf4caa04c24d2e156bc9b3c4
Ivan <vanya.korobov@yandex.ru>
2024-04-30 16:23:00 +0300

    First Commit

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -11,13 +11,11 @@ public class App {
         HelloWorld helloWorld1 = (HelloWorld) applicationContext.getBean("helloworld");
         HelloWorld helloWorld2 = (HelloWorld) applicationContext.getBean("helloworld");
 
-        System.out.println("Comparing HelloWorld beans:");
-        System.out.println("helloWorld1 == helloWorld2: " + (helloWorld1 == helloWorld2));
+        System.out.println("Результат сравнения helloWorld1 и helloWorld2: " + (helloWorld1 == helloWorld2));
 
         Cat cat1 = (Cat) applicationContext.getBean("cat");
         Cat cat2 = (Cat) applicationContext.getBean("cat");
 
-        System.out.println("\nComparing Cat beans:");
-        System.out.println("cat1 == cat2: " + (cat1 == cat2));
+        System.out.println("Результат сравнения cat1 и cat2: " + (cat1 == cat2));
     }
 }
